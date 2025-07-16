237f3410e956f72e666280d6eb0634a368cf08c8
Jeyong <dlwpdyd201@naver.com>
2025-07-16 10:56:37 +0900

    [REFACTOR] 병합 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,22 @@
-
-import React from 'react'
-import TopControls from './TopControls'
-import {useNavigate} from 'react-router-dom';
-import RestaurantBody from './Restaruant/RestaurantBody';
-
+import React from "react";
+import TopControls from "./TopControls";
+import { navigate } from "react-router-dom";
+import RestaurantBody from "./restaurants/RestaurantBody.jsx";
 
 const Home = () => {
-  
-    return (
-        <div className="text-gray-800">
-            <div className="max-w-5xl mx-auto px-4 mt-8">
-                <button
-                    onClick={() => navigate('/register')}
-                    className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer">
-                    맛집 등록
-                </button>
-                <RestaurantBody/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="text-gray-800">
+      <div className="max-w-5xl mx-auto px-4 mt-8">
+        <button
+          onClick={() => navigate("/form")}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer"
+        >
+          맛집 등록
+        </button>
+        <RestaurantBody />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/TopControls.jsx
+++ src/components/TopControls.jsx
@@ -1,28 +1,30 @@
-import {useNavigate} from 'react-router-dom';
-import RatingFilter from './RatingFilter';
-import DistanceFilter from './DistanceFilter';
+import { useNavigate } from "react-router-dom";
+import RatingFilter from "./restaurants/RatingFilter.jsx";
+import DistanceFilter from "./restaurants/DistanceFilter.jsx";
 
 const TopControls = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div
-            id="top-control"
-            className="flex items-center justify-between max-w-xl mx-auto px-4 mt-8 mb-4">
-            {/* 왼쪽: 등록 버튼 */}
-            <button
-                onClick={() => navigate('/register')}
-                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer">
-                맛집 등록
-            </button>
+  return (
+    <div
+      id="top-control"
+      className="flex items-center justify-between max-w-xl mx-auto px-4 mt-8 mb-4"
+    >
+      {/* 왼쪽: 등록 버튼 */}
+      <button
+        onClick={() => navigate("/register")}
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer"
+      >
+        맛집 등록
+      </button>
 
-            {/* 오른쪽: 필터 2개 */}
-            <div className="flex items-center gap-4">
-                <RatingFilter/>
-                <DistanceFilter/>
-            </div>
-        </div>
-    );
+      {/* 오른쪽: 필터 2개 */}
+      <div className="flex items-center gap-4">
+        <RatingFilter />
+        <DistanceFilter />
+      </div>
+    </div>
+  );
 };
 
 export default TopControls;
