44a3e20339369cbfb803d0e3af990fcc11b01dc8
kimhm0828 <kimhm0828@gmail.com>
2025-07-16 10:00:19 +0900

    [FEAT] 데이터 구성 변경에 따른 RestarurantItem의 별점/도보시간 표시 수정

--- src/components/Restaruant/RestaurantItem.jsx
+++ src/components/Restaruant/RestaurantItem.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import IconButton from '../ui/IconButton'
 
+const STAR_MAP = {
+    1 : "⭐️",
+    2 : "⭐️⭐️",
+    3 : "⭐️⭐️⭐️",
+    4 : "⭐️⭐️⭐️⭐️",
+    5 : "⭐️⭐️⭐️⭐️⭐️"
+}
+
+const WALKING_TIME_MAP = {
+    IN_SAIT: "상암IT 타워 내",
+    IN_FIVE_MINUTES: "걸어서 5분 이내",
+    IN_TEN_MINUTES: "걸어서 10분 이내",
+    TEN_MINUTES_OVER: "10분 초과"
+}
+
 function RestaurantItem({foodInfo}) {
 
     return (
@@ -20,27 +35,24 @@ function RestaurantItem({foodInfo}) {
 
         <div className="sm:w-60 p-4">
             <h2 className="text-2xl font-semibold text-gray-900 mb-2">{foodInfo.name}</h2>
-            <div className="flex flex-wrap gap-2 mb-3">
+
+          <p className="text-gray-700 mb-1">
+            <span className="font-medium">평점:</span> {STAR_MAP[foodInfo.rating]}
+          </p>
+
+          <p className="text-gray-700 text-sm">
+            <span className="font-medium">도보 시간:{' '}</span>
+                {WALKING_TIME_MAP[foodInfo.walkingTime]}
+            </p>
+
+             <div className="flex flex-wrap gap-2 mt-3">
                 {foodInfo.tag.map((tag, idx) => (
                     <span key={idx}
                         className="bg-blue-100 text-blue-800 text-sm font-medium px-2.5 py-0.5 rounded-full">
                         #{tag}
                     </span>
                 ))}
-          </div>
-
-          <p className="text-gray-700 mb-1">
-            <span className="font-medium">평점:</span> {foodInfo.rating} ⭐
-          </p>
-
-          <p className="text-gray-700 text-sm">
-            <span className="font-medium">도보 시간:{' '}</span>
-            {foodInfo.walkingTime === 'within5'
-              ? '5분 이내'
-              : foodInfo.walkingTime === 'within10'
-              ? '10분 이내'
-              : '10분 이상'}
-            </p>
+            </div>
         </div>
     </div>
   )
